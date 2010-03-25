9584ca223553ad1480b64b94963bec27e37418f8
Lyndon Maydwell <maydwell@gmail.com>
2010-03-25 21:38:46 +0800

    fixing a typo

--- pinocchio.rb
+++ pinocchio.rb
@@ -11,8 +11,8 @@ enable :method_override, :clean_trace
 set :public,  'public'
 set :views,   'views'
 
-mime_type :css, 'text/css'
-mime_type :js,  'text/javascript'
+mime_type(:css, 'text/css')
+mime_type(:js,  'text/javascript')
 
 error Exception do
 	haml :error
