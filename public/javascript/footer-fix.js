5d2b6c933f505f9180c9da13c7650988e7c08b3d
Lyndon Maydwell <maydwell@gmail.com>
2010-03-25 14:34:39 +0800

    footer fixing madness

--- public/javascript/footer-fix.js
+++ public/javascript/footer-fix.js
@@ -4,12 +4,14 @@
 $(function() {
 
 	var footer = $("#footer")
+	var body = $("#body")
 
 	var offset = footer.offset().top + footer.outerHeight()
 	var difference = body.outerHeight() - offset
 
+	alert(difference)
 
 	if(difference > 0) {
-		#("#content").css({"padding-bottom": "+"+difference+"px"})
+		$("#content").css({"padding-bottom": "+"+difference+"px"})
 	}
 })
